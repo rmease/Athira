e2691ad04ce566ccff202253c675f3fa159da5d9
Ryan Mease <rsmease@gmail.com>
2023-01-27 16:28:22 -0800

    attempt fix of db migration

--- db/migrate/20180720191751_update_headline_for_image_carousels.rb
+++
@@ -1,5 +0,0 @@
-class UpdateHeadlineForImageCarousels < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :image_carousels, :headline, :headlines
-  end
-end
--- db/migrate/20180720200052_add_headlines_to_image_carousels.rb
+++
@@ -1,5 +0,0 @@
-class AddHeadlinesToImageCarousels < ActiveRecord::Migration[5.1]
-  def change
-    # add_column :image_carousels, :headlines, :string, array: true, null: false, default: []
-  end
-end
