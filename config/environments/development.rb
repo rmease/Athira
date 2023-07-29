e07a28ad62a7a2f29a2b692fba4d598e199a4c28
Ryan Mease <rsmease@gmail.com>
2023-07-29 12:14:24 -0700

    updated configurtion of S3 image hosting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,14 @@ Rails.application.configure do
   #   openssl_verify_mode: 'none'
   # }
 
+  # load local environment variables from config/local_env.yml
+  config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
+  end
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
